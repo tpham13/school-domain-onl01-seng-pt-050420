c931a5db6f8205ac07c7439cda053d3d7740a098
Thanh Pham <tpham0123@gmail.com>
2020-05-22 02:12:27 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,33 @@
 # code here!
+
+
+class School 
+  attr_accessor :roster
+  
+  
+  def initialize(school)
+    @roster = {}
+    @school = school 
+  end 
+  
+  def add_student(name, grade)
+    if @roster[grade] 
+    @roster[grade] << name 
+   
+   else 
+    @roster[grade] = []
+    @roster[grade] << name 
+  end 
+  end 
+  
+  def grade(num)
+    @roster[num]
+  end
+  
+  def sort
+    @roster.each do |grade, name|
+    
+    @roster[grade]  = name.sort 
+  end
+  end
+end 
